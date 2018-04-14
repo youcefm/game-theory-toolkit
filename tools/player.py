970b287c70fb00c9120f18d0596ea11f2eaf6996
Youcef Msaid <ym256@cornell.edu>
2018-04-13 22:40:31 -0400

    enrich player class with type and beliefs

--- tools/player.py
+++ tools/player.py
@@ -11,18 +11,26 @@ class Player(object):
 						   user, outside of the Player class. Examples are provides in examples.py
 		-analytic_best_response: if provided, this method is used to calculate the best response set to a profile. The profile (list) is a required argument.
 								 If not provided, the best response set is calculated through maximization over all possible profiles.
+		-type: the type of the player instance, for bayesian games. the framework uses Harsanyi's formulation of player types in strategic form.
+		-beliefs: player's beliefs about the distribution of types of his opponents. If provided, a type must be provided as well.
+		
 		methods:
-		-evaluate_payoff: uses the provided utility_function to return a player's payoff (scalar) from a specific strategy profile 
-		-best_response_set: method that returns the best response set (list) against a particular profile from other players
+		-evaluate_payoff: uses the provided utility_function to return a player's payoff (scalar) from a specific strategy profile. 
+		-best_response_set: method that returns the best response set (list) against a particular profile from other players. 
+							Also writes the output in the current_best_response attribute.
 	"""
-	def __init__(self, position, strategies, utility_function, analytic_best_response=None):
+	def __init__(self, position, strategies, utility_function, analytic_best_response=None, type=None, beliefs=None):
 		self.position = position
 		self.strategies = strategies
 		self.utility_function = utility_function
 		self.analytic_best_response = analytic_best_response
+		self.beliefs = beliefs
 
 	def evaluate_payoff(self, player_strategy, other_players_strategy_profile):
-		return self.utility_function(np.array(player_strategy), np.array(other_players_strategy_profile))
+		if self.beliefs:
+			return self.utility_function(np.array(player_strategy), np.array(other_players_strategy_profile), self.type, self.beliefs)
+		else:
+			return self.utility_function(np.array(player_strategy), np.array(other_players_strategy_profile))
 
 	def best_response_set(self, profile):
 		if self.analytic_best_response:
@@ -31,6 +39,7 @@ class Player(object):
 			possible_set = {strategy:self.evaluate_payoff(strategy, profile) for strategy in self.strategies}
 			best_payoff = max(possible_set.values())
 			best_response_set = [key for key in possible_set if possible_set[key]==best_payoff]
+			self.current_best_response = best_response_set
 		return best_response_set
 
 if __name__ == '__main__':
