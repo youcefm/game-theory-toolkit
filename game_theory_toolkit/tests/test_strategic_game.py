7ef86b04105a340790623ed7741a1b0359e20a27
Youcef Msaid <ym256@cornell.edu>
2018-05-20 11:08:58 -0400

    add feature to unstuck fixed point convergence

--- game_theory_toolkit/tests/test_strategic_game.py
+++ game_theory_toolkit/tests/test_strategic_game.py
@@ -1,11 +1,26 @@
 import numpy as np
-#from game_theory_toolkit.game import StrategicGame
-#from player import Player
-import game_theory_toolkit
+#from ..game import StrategicGame
+#from ..player import Player
+#import game_theory_toolkit
 
-players ={1: Player(1, [1,2,3], lambda x: x), 2: Player(1, [1,2,3], lambda x: x)}
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+from game import StrategicGame
+from player import Player
+
+s = [1,2,3]
+p =[1.0/3, 1.0/3, 1.0/3]
+players ={1: Player(1, p, lambda x: x), 2: Player(1, p, lambda x: x)}
 
 g = StrategicGame(players=players)
+print 'What is this object:', str(g)
+print 'New function: ', g.UniformlyRandomStrategy()
 c = g.contingencies()
+l = []
 for el in c:
-	print el
+	l.append(list(el))
+print 'Manual calculation: ', l
+print l[1][1]
